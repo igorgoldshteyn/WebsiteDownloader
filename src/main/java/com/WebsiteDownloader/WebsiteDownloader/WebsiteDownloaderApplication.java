7ea1bda270de50c2d52426ce3a4ccb8075c73b70
Igor Goldshteyn <igorgoldshteyn@yandex.ru>
2022-11-20 10:02:23 +0300

    Renamed project

---
+++ src/main/java/com/WebsiteDownloader/WebsiteDownloader/WebsiteDownloaderApplication.java
@@ -0,0 +1,14 @@
+package com.WebsiteDownloader.WebsiteDownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDownloaderApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteDownloaderApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/WebsiteDownloader/WebsiteDownloader/WebsiteDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebsiteDownloader.WebsiteDownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
