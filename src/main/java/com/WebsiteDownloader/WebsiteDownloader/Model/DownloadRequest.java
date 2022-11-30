8b16a3d90ea5c0106c8d5a783f89767230018b6c
Igor Goldshteyn <igorgoldshteyn@yandex.ru>
2022-11-30 10:52:29 +0300

    1

--- src/main/java/com/WebsiteDownloader/WebsiteDownloader/Model/DownloadRequest.java
+++ src/main/java/com/WebsiteDownloader/WebsiteDownloader/Model/DownloadRequest.java
@@ -1,9 +1,11 @@
 package com.WebsiteDownloader.WebsiteDownloader.Model;
 
+import java.time.LocalDateTime;
 
 public class DownloadRequest {
 
     private Integer id;
     private String requestUrl;
     private User user ;
-    private Date date;
+    private LocalDateTime  date;
+}
---
+++ src/main/java/com/WebsiteDownloader/WebsiteDownloader/Services/LoginService.java
@@ -0,0 +1,12 @@
+package com.WebsiteDownloader.WebsiteDownloader.Services;
+
+import com.WebsiteDownloader.WebsiteDownloader.WebsiteDownloaderApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+class LoginService{
+
+
+}
