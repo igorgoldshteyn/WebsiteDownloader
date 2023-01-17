e6455646ff86f0510b6c4ddc683ed33dad220d96
linux778 <igorgoldshteyn@yandex.ry>
2023-01-17 13:57:21 +0300

    Optimized file download.

--- src/main/java/com/WebsiteDownloader/Services/DownloadService.java
+++ src/main/java/com/WebsiteDownloader/Services/DownloadService.java
@@ -4,6 +4,7 @@ import com.WebsiteDownloader.WebsiteDownloaderApplication;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ContentDisposition;
@@ -24,55 +25,24 @@ import java.nio.file.Paths;
 
 @Service
 public class DownloadService {
-    private static Logger logger = LoggerFactory.getLogger(WebsiteDownloaderApplication.class);
 
-
-    public static ResponseEntity<Resource> downloadByUrl(String url) throws IOException, InterruptedException {
-        HttpResponse<String> response = null;
+    public static ResponseEntity<byte[]> downloadByUrl(String url) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
+        HttpResponse<String> response = null;
         try {
-
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("response = " + response.body());
-
         } catch (IOException e) {
             System.out.println(e);
         }
 
-        // creating a file in a path
-
-        File f = new File("src/main/resources/Files/1.txt");
-        f.createNewFile(); // сделать проверку, что файл не создан и именование правильное
-
-
-        FileOutputStream outputStream = new FileOutputStream(f);
-        byte[] strToBytes = response.body().getBytes();
-        outputStream.write(strToBytes);
-
-        outputStream.close();
-
-        Path filePath = Path.of("1.txt");
-        Resource resource = new UrlResource(filePath.toUri());
-
-        System.out.println("SSSSSSSSSSSSSSSSSSSS");
-
-        String content = Files.readString(filePath);
-
-        System.out.println("AAAAAAAAAAAAAAAA");
-
-
-        System.out.println();
-
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename(resource.getFilename()).build().toString()); // (4) Content-Disposition: attachment; filename="demo-file.txt"
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("1.html").build().toString());
 
-        return ResponseEntity.ok().headers(httpHeaders)
-                .body(resource);
-
+        return ResponseEntity.ok().headers(httpHeaders).body(response.body().getBytes());
 
     }
 }
